93c93998a83160c9524b395e4fa7c5de6c2c30c1
Dca <dcahsu@gmail.com>
2018-01-16 00:50:09 +0800

    feat(command): add `/give_me_id` command

--- README.md
+++ README.md
@@ -7,3 +7,8 @@
 - Line Messaging API Channel
 - Edit and mv `.env.example` to `.env`
 - RUN `npm install` and `npm run start`
+
+
+### how to get your `LINE_GROUP_ID` in .env file?
+
+run this project and send `/give_me_id` to your bot.
--- src/service/linebot.js
+++ src/service/linebot.js
@@ -20,6 +20,19 @@ bot.on('message', async function (event) {
     console.log('profile', await event.source.profile())
     console.log('replyMsg', replyMsg)
 
+    if (msg === '/give_me_id') {
+      let groupId = event.source.groupId
+      let userId = event.source.userId
+      let replyMsg = `* groupId: ${groupId}\n* userId: ${userId}`
+      event.reply(replyMsg).then(function (data) {
+        console.log(replyMsg)
+      }).catch(function (error) {
+        if (error) {
+          console.log('error')
+        }
+      })
+    }
+
     // 先拔掉這個功能避免在群組太吵
     // event.reply(replyMsg).then(function (data) {
     //   console.log(replyMsg)
