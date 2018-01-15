d791bd0f99156ace714000ff7b0331dbec83f35f
Dca <dcahsu@gmail.com>
2018-01-15 13:00:04 +0800

    Update router.js

--- src/router.js
+++ src/router.js
@@ -4,6 +4,11 @@ const router = require('koa-router')({
 const linebotKoaMiddleware = require('linebot-koa-middleware')
 const linebot = require('./service/linebot')
 
+
+
+router.get('/', (ctx) => {
+  ctx.redirect('https://www.facebook.com/groups/node.js.tw')
+});
 router.post('/github/webhook', require('./route/github_callback'))
 router.post('/zapier/webhook', require('./route/zapier_callback'))
 router.post('/line/webhook', linebotKoaMiddleware(linebot))
