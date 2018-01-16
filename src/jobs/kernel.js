b69094b7d7a291cea6d672b39ab4e97febf3c11c
Dca <dcahsu@gmail.com>
2018-01-16 19:20:41 +0800

    feat(jobs): close issue once a day

--- src/jobs/kernel.js
+++ src/jobs/kernel.js
@@ -1,6 +1,8 @@
+const log = require('../utils/log')('ntb:jobs:kernel')
 const schedule = require('node-schedule')
 const autoClosedIssue = require('./autoClosedIssue')
 
-schedule.scheduleJob('* * * * *', function () {
+schedule.scheduleJob('09 20 * * *', function () {
+  log.debug('autoClosedIssue')
   autoClosedIssue()
 })
